a77b38d5c2aa8af0de0b130422dfc401d0f2d41d
Faizal <faizalmalik9633@gmail.com>
2019-03-06 13:20:17 +0530

    [Feature] - Implemented Search Functionality on Users List

--- AlamofireExample/AlamofireExample/ViewControllers/ViewController.swift
+++ AlamofireExample/AlamofireExample/ViewControllers/ViewController.swift
@@ -12,14 +12,49 @@ class HomeViewController: UIViewController {
 
     @IBOutlet weak var tableViewHome: UITableView!
     var usersArray:[User]? = []
+    var filterdusersArray:[User]? = []
     
-    
+    let refreshControl = UIRefreshControl()
+    var isFiltered = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         fetchData()
+        setupView()
     }
+    
+    func setupView(){
+        //Here we set the View related Codes
+        
+        //Set Refresh controller
+        refreshControl.tintColor = UIColor.clear
+        refreshControl.attributedTitle = NSAttributedString(string: "")
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tableViewHome.addSubview(refreshControl)
+        
+        //Set Search bar Header
+        
+        let searchBar = UISearchBar()
+        searchBar.frame = CGRect(x: 0, y: 0, width: self.tableViewHome.frame.width, height: 50)
+        searchBar.delegate = self
+        searchBar.placeholder = "Search user"
+        self.tableViewHome.tableHeaderView = searchBar
+        
+        
+       
+    }
+    
+    
+    @objc func refresh(sender:AnyObject) {
+        
+      refreshControl.endRefreshing()
+       
+            fetchData()
+       
+        
+    }
+    
     
     
     //MARK: Other Methods
@@ -51,28 +86,77 @@ class HomeViewController: UIViewController {
         }
         
     }
+    
 }
 
 //MARK: UITableViewDataSource Methods
 extension HomeViewController : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (self.usersArray?.count)!
+        return isFiltered ?  (self.filterdusersArray?.count)! : (self.usersArray?.count)!
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
             
             let cell = tableView.dequeueReusableCell(withIdentifier: "ListTableViewcell") as! ListTableViewcell
-        let user = usersArray![indexPath.row]
+        
+        let user = isFiltered ? filterdusersArray![indexPath.row] : usersArray![indexPath.row]
         
          cell.lblName.text = (user.name?.first)! + " " +  (user.name?.last)!
          cell.lblEmail.text = user.email
         cell.imgUser.imageFromServerURL((user.picture?.thumbnail)!,placeHolder: UIImage(named: "placeholder"))
+        
         return cell
     }
     
     
     
+}
+
+extension HomeViewController : UISearchBarDelegate {
+    
+    //MARK: Other Methods
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        
+        if searchText.count == 0 {
+            isFiltered = false
+        }
+        else{
+            isFiltered = true
+           
+            self.filterdusersArray = self.usersArray!.filter({ (item) -> Bool in
+                (item.name?.first?.contains(searchText.lowercased()))! || (item.name?.last?.contains(searchText.lowercased()))!
+            })
+            
+            
+            
+            
+        }
+        
+        
+        self.tableViewHome.reloadData()
+        
+        
+    }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = false
+        
+        
+    }
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        searchBar.showsCancelButton = true
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.showsCancelButton = false
+        searchBar.text = ""
+        isFiltered = false
+        tableViewHome.reloadData()
+        
+    }
 }
 class ListTableViewcell : UITableViewCell {
     
@@ -86,4 +170,3 @@ class ListTableViewcell : UITableViewCell {
         
     }
 }
-
